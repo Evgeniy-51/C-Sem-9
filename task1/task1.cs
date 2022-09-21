3a1cecf5f7da3bc7fa2e74b1e2bc63810f2d30c1
Evgeniy-51 <evgeniy-51@yandex.ru>
2022-09-21 22:51:36 +0300

    task2

---
+++ task1/task1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+// Задача 1: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+
+Console.Clear();
+
+Random rnd = new Random();
+int lines = rnd.Next(3, 9);
+int columns = rnd.Next(3, 9);
+int minLim = 0;
+int maxLim = 99;
+int[,] arr = new int[lines, columns];
+
+for (int i = 0; i < lines; i++)  // Заполнение массива случайными числами
+{
+    int minVal = minLim;
+    for (int j = 0; j < columns; j++)
+    {
+        arr[i, j] = rnd.Next(minLim, maxLim);
+        Console.Write(arr[i, j] + "\t");
+    }
+    System.Console.WriteLine();
+}
+Console.WriteLine(new string('_', columns * 8));
+
+int temp = 0;
+for (int i = 0; i < lines; i++)  // Пузырьковая сортировка
+{
+    for (int j = 0; j < columns; j++)
+    {
+        for (int k = 0; k < columns - j - 1; k++)
+        {
+            if (arr[i, k] < arr[i, k + 1])
+            {
+                temp = arr[i, k];
+                arr[i, k] = arr[i, k + 1];
+                arr[i, k + 1] = temp;
+            }
+        }
+    }
+}
+
+for (int i = 0; i < lines; i++)  // Вывод массива
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write(arr[i, j] + "\t");
+    }
+    System.Console.WriteLine();
+}
---
+++ task2/task2.cs
@@ -0,0 +1,50 @@
+﻿// Задача 2**. Напишите программу, которая спирально заполнит числами от 1 до 16 двумерный массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+Console.Clear();
+
+int size = 4;
+int area = size;
+int[,] arr = new int[size, size];
+int num = 1;
+
+for (int i = 0; i < size / 2; i++)
+{
+    for (int k = i; k < area - 1; k++)
+    {
+        arr[i, k] = num;
+        num++;
+    }
+    for (int k = i; k < area - 1; k++)
+    {
+        arr[k, area - 1] = num;
+        num++;
+    }
+    for (int k = area - 1; k > i; k--)
+    {
+        arr[area - 1, k] = num;
+        num++;
+    }
+    for (int k = area - 1; k > i; k--)
+    {
+        arr[k, i] = num;
+        num++;
+    }
+    area--;
+}
+
+for (int i = 0; i < size; i++)  // Вывод массива
+{
+    for (int j = 0; j < size; j++)
+    {
+        Console.Write(arr[i, j] + "\t");
+    }
+    System.Console.WriteLine();
+}
+
+
+
